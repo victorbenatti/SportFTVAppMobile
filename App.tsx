6e93c3a23198e2e0a172e536233c91045a16e600
Victor Benatti <victorbenatti13@gmail.com>
2025-08-26 15:26:09 -0300

    feat: adiciona bootsplash com logo e cor de fundo personalizada
    - Configura react-native-bootsplash com logo.png
    - Define cor de fundo azul (#1E3A8A) para splash screen
    - Gera assets para Android (múltiplas densidades)
    - Gera assets para iOS (múltiplas resoluções)
    - Adiciona configurações de cores e estilos necessários

--- App.tsx
+++ App.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import AppNavigator from './src/navigation/AppNavigator';
 import { UserProvider } from './src/context/UserContext';
+import RNBootSplash from 'react-native-bootsplash';
 
-function App(): React.JSX.Element {
+const App = () => {
+  useEffect(() => {
+    // Esconde a splash screen quando o componente App é montado
+    RNBootSplash.hide({ fade: true }); 
+  }, []);
+
   return (
     <UserProvider>
       <AppNavigator />
     </UserProvider>
   );
-}
+};
 
 export default App;
